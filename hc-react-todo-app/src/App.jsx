9111c25e05041470d042c5d9c3abeccb07552645
arunbababa <hatuki.1.gzs@icloud.com>
2026-02-22 18:05:21 +0900

    create todo app with react

--- hc-react-todo-app/src/App.jsx
+++ hc-react-todo-app/src/App.jsx
@@ -1,33 +1,88 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todoInput, setTodoInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editIndex, setEditIndex] = useState(null);
+  const [editInput, setEditInput] = useState("");
+
+  const onChangeTodoInput = (e) => {
+    setTodoInput(e.target.value)
+  }
+
+  const onChangeEditInput = (e) => {
+    setEditInput(e.target.value)
+  }
+
+  const onClickAddTodo = () => {
+    const newTodos = [...todos, todoInput]
+    setTodos(newTodos)
+    setTodoInput("")
+  }
+
+  const onCLickRenameTodo = (index) => {
+    setEditIndex(null)
+    const newTodos = [...todos]
+    newTodos.splice(index, 1, editInput)
+    setTodos(newTodos)
+  }
+
+  const handleEditToggle = (todo, index) => {
+    if (editIndex === index){
+      setEditIndex(null)
+      return;
+    }
+    setEditIndex(index);
+    setEditInput(todo);
+  }
+
+  const onClickDeleteTodo = (todo, index) => {
+    if (!confirm(`${todo}を削除しますか？`)) return;
+
+    const newTodos = [...todos]
+    newTodos.splice(index, 1)
+    setTodos(newTodos)
+  }
 
   return (
     <>
+      <h1>Reactで作るタスク管理アプリ</h1>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input type="text" placeholder='タスクを入力してください' onChange={onChangeTodoInput} value={todoInput}/>
+        <button onClick={onClickAddTodo}>追加</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="todo-list">
+        <h3>TODOリスト</h3>
+        <ul>
+          {
+            todos.map((todo, index) => (
+              <li key={index}>
+                <div className='todo-item'>
+                  {
+                    editIndex === index ? (
+                      <>
+                        <input type="text" value={editInput} onChange={onChangeEditInput}/>
+                        <button onClick={() => onCLickRenameTodo(index)}>保存</button>
+                        <button onClick={() => handleEditToggle(todo, index)}>キャンセル</button>
+                      </>
+                    ):
+                    (
+                      <>
+                        <p>{todo}</p>
+                        <button onClick={() => handleEditToggle(todo, index)}>編集</button>
+                        <button onClick={() => onClickDeleteTodo(todo, index)}>削除</button>
+                      </>
+                    )
+                  }
+                  
+                </div>
+              </li>
+            ))
+          }
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
