3918b7ff6bcdbf44b856f3d407ef1fe127fbd425
arunbababa <hatuki.1.gzs@icloud.com>
2026-02-23 17:16:59 +0900

    feat: ビルド出力先をリポジトリルートに変更し最新ビルドを反映

--- hc-react-todo-app/vite.config.js
+++ hc-react-todo-app/vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/react_todo_list_/',
+  build: {
+    outDir: '../',
+    emptyOutDir: false,
+  },
 })
